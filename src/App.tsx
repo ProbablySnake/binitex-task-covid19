2235c3cd9c097e6c0eb2ba4e418a86dddb6c4a67
Arturs Volkausks <artja201@yandex.com>
2022-07-20 09:29:22 +0300

    Fixed variable types in data

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,27 @@ export interface IAppProps {
 
 export interface IAppState {
   DataisLoaded: boolean,
-  items?: { records: [] }
+  items?: { records: Array<any> },
+  data?: {
+    records: Array<Record>,
+    minDate: Date,
+    maxDate: Date,
+  }
+}
+
+interface Record {
+  dateRep: Date,
+  day: number,
+  month: number,
+  year: number,
+  cases: number,
+  deaths: number,
+  countriesAndTerritories: string,
+  geoId: string,
+  countryterritoryCode: string,
+  popData2019: number,
+  continentExp: string,
+  'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000': number,
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
@@ -22,28 +42,49 @@ export default class App extends React.Component<IAppProps, IAppState> {
       "/covid19/casedistribution/json")
       .then((res) => res.json())
       .then((json) => {
+        let records: Record[] = [];
+        let minDate = new Date(json.records[0].year, json.records[0].month - 1, json.records[0].day);
+        let maxDate = new Date(json.records[0].year, json.records[0].month - 1, json.records[0].day);
+        json.records.map((rawRecord: any, id: number) => {
+          records.push({
+            dateRep: new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day),
+            day: Number(rawRecord.day),
+            month: Number(rawRecord.month),
+            year: Number(rawRecord.year),
+            cases: rawRecord.cases,
+            deaths: rawRecord.deaths,
+            countriesAndTerritories: rawRecord.countriesAndTerritories,
+            geoId: rawRecord.geoId,
+            countryterritoryCode: rawRecord.countryterritoryCode,
+            popData2019: rawRecord.popData2019,
+            continentExp: rawRecord.continentExp,
+            'Cumulative_number_for_14_days_of_COVID-19_cases_per_100000': Number(rawRecord['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']),
+          });
+          if (minDate > new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)) {
+            minDate = new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)
+          }
+          if (maxDate < new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)) {
+            maxDate = new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)
+          }
+        })
+
         this.setState({
-          items: json,
+          data: { records: records, minDate: minDate, maxDate: maxDate },
           DataisLoaded: true
         });
-        console.log(json);
       })
   }
 
   render() {
-    const { DataisLoaded, items } = this.state;
+    const { DataisLoaded, data } = this.state;
     if (!DataisLoaded) return <div>
       <h1> Pleses wait some time.... </h1> </div>;
 
+    console.log(data);
+
     return (
       <div className="App">
-        <h1> Fetch data from an api in react </h1>  {
-          items?.records.map((item: { cases: number }, id) => (
-            <ol key={id}>
-              {item.cases}
-            </ol>
-          ))
-        }
+        <h1> Fetch data from an api in react </h1>
       </div>
     );
   }
