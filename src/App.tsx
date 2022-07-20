8eed3c221425d9c34ed4f956108c347f796ab651
Arturs Volkausks <artja201@yandex.com>
2022-07-20 07:40:06 +0300

    Deleted unnecessary files from create-react-app, and made fetch to the API + cors proxy

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
+
+export interface IAppProps {
 }
 
-export default App;
+export interface IAppState {
+  DataisLoaded: boolean,
+  items?: { records: [] }
+}
+
+export default class App extends React.Component<IAppProps, IAppState> {
+
+  state: IAppState = {
+    DataisLoaded: false
+  };
+
+  componentDidMount() {
+    fetch(
+      "/covid19/casedistribution/json")
+      .then((res) => res.json())
+      .then((json) => {
+        this.setState({
+          items: json,
+          DataisLoaded: true
+        });
+        console.log(json);
+      })
+  }
+
+  render() {
+    const { DataisLoaded, items } = this.state;
+    if (!DataisLoaded) return <div>
+      <h1> Pleses wait some time.... </h1> </div>;
+
+    return (
+      <div className="App">
+        <h1> Fetch data from an api in react </h1>  {
+          items?.records.map((item: { cases: number }, id) => (
+            <ol key={id}>
+              {item.cases}
+            </ol>
+          ))
+        }
+      </div>
+    );
+  }
+}
