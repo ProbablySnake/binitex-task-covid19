1aa256f7809a7eb9ee9225ef23671288d83116b3
Arturs Volkausks <artja201@yandex.com>
2022-07-21 18:33:41 +0300

    Moved data processing to utils.tsx Improved data processing

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
-import { stringify } from 'querystring';
 import React from 'react';
 
+import { prcocessDataFromJson } from './utils';
 
 export interface IAppProps {
 }
 
 export interface IAppState {
   DataisLoaded: boolean,
-  items?: { records: Array<any> },
   data?: {
-    records: Array<Record>,
     minDate: Date,
     maxDate: Date,
     countries: string[],
+    records: Record[],
+    recordsWorldwide: Record[],
   }
 }
 
@@ -23,7 +23,7 @@ export interface Record {
   casesTotal: number,
   deaths: number,
   deathsTotal: number,
-  cumulativeCases: number,
+  popData2019: number,
 }
 
 export default class App extends React.Component<IAppProps, IAppState> {
@@ -33,99 +33,11 @@ export default class App extends React.Component<IAppProps, IAppState> {
   };
 
   componentDidMount() {
-    fetch(
-      "/covid19/casedistribution/json")
+    fetch("/covid19/casedistribution/json") // Getting data from API   https://opendata.ecdc.europa.eu added as proxy in package.json to 'fix' CORS bug
       .then((res) => res.json())
       .then((json) => {
-
-        let records: Record[] = [];
-        let minDate = new Date(json.records[0].year, json.records[0].month - 1, json.records[0].day);
-        let maxDate = new Date(json.records[0].year, json.records[0].month - 1, json.records[0].day);
-        let casesCounter: any = {};
-        let deathsCounter: any = {};
-        let countries: string[];
-        let prevDate: Date;
-        let storedId: number;
-
-        json.records.map((rawRecord: any) => {
-          records.push({
-            date: new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day),
-            cases: rawRecord.cases,
-            casesTotal: 0,
-            deaths: rawRecord.deaths,
-            deathsTotal: 0,
-            country: rawRecord.countriesAndTerritories,
-            cumulativeCases: Number(rawRecord['Cumulative_number_for_14_days_of_COVID-19_cases_per_100000']),
-          });
-          if (minDate > new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)) {
-            minDate = new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)
-          }
-          if (maxDate < new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)) {
-            maxDate = new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day)
-          }
-          casesCounter[rawRecord.countriesAndTerritories] = 0;
-          deathsCounter[rawRecord.countriesAndTerritories] = 0;
-        })
-
-        countries = Object.getOwnPropertyNames(casesCounter);
-
-        records.sort((a: Record, b: Record) => {
-          if (a.date < b.date) return -1
-          if (a.date > b.date) return 1
-          return 0
-        })
-
-        records.map((record: Record, id: number) => {
-
-          casesCounter[record.country] += record.cases;
-          deathsCounter[record.country] += record.deaths;
-          records[id].casesTotal = casesCounter[record.country];
-          records[id].deathsTotal = deathsCounter[record.country];
-
-          if (prevDate !== undefined && prevDate.getTime() === record.date.getTime()) {
-            records[storedId] = {
-              country: 'World',
-              date: record.date,
-              cases: records[storedId].cases + record.cases,
-              deaths: records[storedId].deaths + record.deaths,
-              cumulativeCases: records[storedId].cumulativeCases + record.cumulativeCases,
-              casesTotal: records[storedId].casesTotal + casesCounter[record.country],
-              deathsTotal: records[storedId].deathsTotal + deathsCounter[record.country],
-            }
-          } else {
-            if (storedId) records[storedId].cumulativeCases = records[storedId].cumulativeCases / countries.length;
-            storedId = records.push({
-              country: 'World',
-              date: record.date,
-              cases: record.cases,
-              deaths: record.deaths,
-              cumulativeCases: record.cumulativeCases,
-              casesTotal: casesCounter[record.country],
-              deathsTotal: deathsCounter[record.country],
-            }) - 1;
-            prevDate = record.date;
-          }
-
-        });
-
-        records.sort((a: Record, b: Record) => {
-          if (a.country < b.country) return -1
-          if (a.country > b.country) return 1
-          if (a.date < b.date) return -1
-          if (a.date < b.date) return 1
-          return 0
-        })
-
-        countries.sort();
-        countries.unshift('World');
-
         this.setState({
-          data: {
-            records: records,
-            minDate: minDate,
-            maxDate: maxDate,
-            countries: countries,
-          },
+          data: prcocessDataFromJson(json),
           DataisLoaded: true
         });
       })
@@ -137,7 +49,7 @@ export default class App extends React.Component<IAppProps, IAppState> {
       <h1> Please wait some time.... </h1> </div>;
 
     if (data)
-    console.log(data);
+    // console.log(data);
 
     return (
       <div className="App">
---
+++ src/utils.tsx
@@ -0,0 +1,84 @@
+import { IAppState, Record } from "./App";
+
+export function prcocessDataFromJson({ records: rawRecords }: { records: Array<any> }): IAppState['data'] {
+
+  // Format Array from JSON
+  let records = rawRecords.map((rawRecord: any): Record => {
+    return {
+      country: rawRecord.countriesAndTerritories,
+      date: new Date(rawRecord.year, rawRecord.month - 1, rawRecord.day),
+      cases: rawRecord.cases,
+      casesTotal: 0,
+      deaths: rawRecord.deaths,
+      deathsTotal: 0,
+      popData2019: rawRecord.popData2019,
+    }
+  });
+
+  // Set up counters for counting total cases/deaths (from day 1 to individual record date)
+  let casesCounter: { [key: string]: number; } = {};
+  records.forEach(element => {
+    casesCounter[element.country] = 0
+  });
+  let deathsCounter = casesCounter;
+
+  let countries = Object.keys(casesCounter) // Getting array of all countries for search aoutocomplete
+
+  // sorting by date
+  records.sort((a: Record, b: Record) => {
+    if (a.date < b.date) return -1
+    if (a.date > b.date) return 1
+    return 0
+  })
+
+  // getting min/max dates for datepicker
+  let minDate = records[0].date;
+  let maxDate = records[records.length - 1].date;
+
+  // actualy counting totals and saving them
+  for (let i = 0; i < records.length; i++) {
+    casesCounter[records[i].country] += records[i].cases;
+    deathsCounter[records[i].country] += records[i].deaths;
+    records[i].casesTotal = casesCounter[records[i].country];
+    records[i].deathsTotal = deathsCounter[records[i].country];
+  }
+
+  // creating separate records table for worldwide statistics
+  let recordsWorldwide: Array<Record> = [];
+
+  let currentDate = new Date(0); // records will be summed by dates, so I have to know currend date
+
+  for (const record of records) {
+    if (currentDate.getTime() !== record.date.getTime()) {  // if new day started pushing new record in array
+      currentDate = record.date; // updating currentDate
+      recordsWorldwide.push({
+        country: 'World',
+        date: record.date,
+        cases: record.cases,
+        casesTotal: record.casesTotal,
+        deaths: record.deaths,
+        deathsTotal: record.deathsTotal,
+        popData2019: record.popData2019,
+      });
+      continue;
+    }
+    let id = recordsWorldwide.length - 1;  // when it's the same day add numbers to existing record
+    recordsWorldwide[id] = {
+      ...recordsWorldwide[id],
+      cases: record.cases + recordsWorldwide[id].cases,
+      casesTotal: record.casesTotal + recordsWorldwide[id].casesTotal,
+      deaths: record.deaths + recordsWorldwide[id].deaths,
+      deathsTotal: record.deathsTotal + recordsWorldwide[id].deathsTotal,
+      popData2019: record.popData2019 + recordsWorldwide[id].popData2019,
+    }
+  }
+
+  //returning data
+  return {
+    minDate,
+    maxDate,
+    countries,
+    records,
+    recordsWorldwide
+  }
+}
